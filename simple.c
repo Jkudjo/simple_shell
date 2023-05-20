6a0445d3d3d8122fe6efa456ba2beb5c34015df4
jkudjo <philipkumahjr@gmail.com>
2023-05-20 03:08:12 -0700

    new simple code

---
+++ simple.c
@@ -0,0 +1,15 @@
+/**
+ * add_numbers - Adds two numbers and returns the result
+ * @a: The first number to be added
+ * @b: The second number to be added
+ *
+ * Return: The sum of the two numbers
+ */
+int add_numbers(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return sum;
+}
+
